1ec41736f6f4686ccb7932c8abeb42aaaf901259
swimmy0429 <yukihiroswimmy@gmail.com>
2020-10-14 14:53:00 +0900

    add_item_val

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,13 @@ class ItemsController < ApplicationController
     @item_images_top = ItemImage.all.includes(:item).group(:item_id)
   end
 
+  def show
+    @items_show = Item.all.where(id:params[:id])
+    @item_images_detail = ItemImage.all.includes(:item).where(item_id:params[:id])
+    # @seller = User.all.where(id:@items_show.seller_id).nickname
+
+  end
+
   def new
     @item = Item.new
     @item.item_images.new
@@ -44,12 +51,5 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
-    @items = Item.all.where.not(trading_status:2)
-    @item_images_top = ItemImage.all.includes(:item).group(:item_id)
-  end
-  def show
-    @items_show = Item.all.where(id:params[:id])
-    @item_images_detail = ItemImage.all.includes(:item).where(item_id:params[:id])
-  end
+  
 end
