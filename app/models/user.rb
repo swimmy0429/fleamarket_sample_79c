ca9352cfb3a4628b3d2f8b0c9decc4240976b88e
swimmy0429 <yukihiroswimmy@gmail.com>
2020-11-17 21:03:38 +0900

    add_comment_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
+  has_many :comments, dependent: :destroy
 
   has_many :favorites
   has_many :items, through: :favorites
