0b7255faa0e03010b5878f59c231caf6a6d3edde
aki-0520 <kido0520@gmail.com>
2020-11-21 16:32:29 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,11 +13,11 @@ class FavoritesController < ApplicationController
   # お気に入り削除
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, item_id: @item.id)
-    @favorite.destroy
-      
-    end
+    @favorite.destroy      
+  end
 
   private
   def set_item
     @item = Item.find(params[:item_id])
+  end
 end
